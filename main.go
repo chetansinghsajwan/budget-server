4d100b47fcc9b25427c5c9a563c85a9a1530f463
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2025-05-06 18:40:56 +0530

    update: add db connect

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"budget-server/db"
+	"budget-server/transaction"
 	"log"
 	"net/http"
-	"budget-server/transaction"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	http.HandleFunc("/transaction", transaction.HandleTransaction)
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("WARNING: Failed to load local env file, err: %s", err.Error())
+	}
 
-	log.Println("Server is running on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	db.Init()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /transaction", transaction.HandleCreateTransaction)
+	mux.HandleFunc("GET /transaction/{id}", transaction.HandleGetTransaction)
+	mux.HandleFunc("PATCH /transaction/{id}", transaction.HandleUpdateTransaction)
+	mux.HandleFunc("DELETE /transaction/{id}", transaction.HandleDeleteTransaction)
+
+	log.Println("Server listening at port 8080...")
+	serverError := http.ListenAndServe(":8080", mux)
+	log.Fatal(serverError)
 }
--- transaction/transaction.go
+++ transaction/transaction.go
@@ -1,41 +1,53 @@
 package transaction
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func HandleCreateTransaction(w http.ResponseWriter, r *http.Request) {
-	log.Println("Create transaction request received.")
+    log.Println("Create transaction request received.")
 }
 
 func HandleDeleteTransaction(w http.ResponseWriter, r *http.Request) {
-	log.Println("Delete transaction request received.")
+    log.Println("Delete transaction request received.")
 }
 
 func HandleUpdateTransaction(w http.ResponseWriter, r *http.Request) {
-	log.Println("Update transaction request received.")
+    log.Println("Update transaction request received.")
 }
 
 func HandleGetTransaction(w http.ResponseWriter, r *http.Request) {
-	log.Println("Get transaction request received.")
+    id, err := strconv.Atoi(r.PathValue("id"))
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    log.Printf("Get transaction request received for id '%d'", id)
+
+    w.Header().Set("content-type", "application/json")
+    w.Write([]byte(fmt.Sprintf("Get transaction request received for id '%d'", id)))
 }
 
 func HandleUnknownTransaction(w http.ResponseWriter, r *http.Request) {
-	log.Println("Unknown transaction request received.")
+    log.Println("Unknown transaction request received.")
 }
 
 func HandleTransaction(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		HandleGetTransaction(w, r)
-	case http.MethodPut:
-		HandleCreateTransaction(w, r)
-	case http.MethodDelete:
-		HandleDeleteTransaction(w, r)
-	case http.MethodPatch:
-		HandleUpdateTransaction(w, r)
-	default:
-		HandleUnknownTransaction(w, r)
-	}
+    switch r.Method {
+    case http.MethodGet:
+        HandleGetTransaction(w, r)
+    case http.MethodPut:
+        HandleCreateTransaction(w, r)
+    case http.MethodDelete:
+        HandleDeleteTransaction(w, r)
+    case http.MethodPatch:
+        HandleUpdateTransaction(w, r)
+    default:
+        HandleUnknownTransaction(w, r)
+    }
 }
